f6bc72ad138ba5778ca693b808321c37cdf28a2c
sean@mbpro_new <tehsheng+mbpro_new@umich.edu>
2018-09-26 16:51:54 -0400

    saving extracted neurite array in dataframe

--- NODDI/NODDI_Extraction.py
+++ NODDI/NODDI_Extraction.py
@@ -46,20 +46,32 @@ def roi_mask(x, y, z, label, size):
 
 # %% looping over dataframe to create & save sphere for roi masked_img_s1 & later fslview
 from nltools.mask import create_sphere
+
+# initialize dataframe
+neurite_df = pd.DataFrame()
+
 # r_mask = create_sphere([32, 24, -11], radius = 15)
 # r_mask.to_filename('15mm_mask.nii')
 # inspired by https://stackoverflow.com/questions/43619896/python-pandas-iterate-over-rows-and-access-column-names
 for row in roi_df.itertuples():
     # saving and returning the roi mask created
     mask = roi_mask(row.x, row.y, row.z, row.label, row.size)
+
     # applying the mask on image of interest
     masked_img_s1 = img_s1.apply_mask(mask)
+
+    # concatenate extracted neurite into dataframe
+    neurite_df = pd.concat([neurite_df, pd.DataFrame(masked_img_s1.data, columns=[row.label])],
+    axis = 1)
+
     # plotting ortho views at roi coordinates on subject's brain instead of default axial plots
     # masked_img_s1.plot()
     plotting.plot_stat_map(masked_img_s1.to_nifti(), bg_img=anat,
     display_mode='ortho', cut_coords=[row.x, row.y, row.z],
     draw_cross=False,
     title="{0}, {1} mm sphere at [{2},{3},{4}]".format(row.label,str(row.size),row.x,row.y,row.z))
+
+neurite_df
 
 # %% plot the masked image with histogram distributions
 import seaborn as sns
