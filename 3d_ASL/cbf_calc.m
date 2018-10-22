088f0fba2edc3ef31c48f16b12155271ff79ce1e
sean@mbpro_new <tehsheng+mbpro_new@umich.edu>
2018-10-22 16:33:02 -0400

    adding write out 2 nii function; add comments from Scott

--- 3d_ASL/cbf_calc.m
+++ 3d_ASL/cbf_calc.m
@@ -1,7 +1,11 @@
-%% Written by Scott Peltier @fMRI Lab
-%% 10/17/2018, 12:36:14 PM
+%%Calculation of quantitative CBF map using GE's 3Dasl sequence.
+% This uses same assumptions, masking and calculations as GE's maps.
+%
+% 2018, Scott Peltier
+% 10/17/2018, 12:36:14 PM
 function [cbfmap]=cbf_calc(asl_filename);
 
+%% Read in data, reshape, do masking of images
 tst=read_nii_img(asl_filename);
 tst=reshape(tst.',[128 128 40 2]);
 
@@ -18,29 +22,28 @@ msk2=(pd<200);  %masks non-brain areas
 % nnz(msk1 == 1)    % 139692
 % nnz(msk2 == 1)    % 409735
 
-%% parameter definitions
-alpha=0.9;
-ST=2;
-T1t=1.2;
-eff=0.6;
-T1b=1.6;
-LT=1.5;
+%% GE-defined parameters
+alpha = 0.9;    %partition coefficient
+ST = 2;         %saturation time in seconds
+T1t = 1.2;      %partial saturation T1 in pd image
+eff = 0.6;      %overall efficiency
+T1b = 1.6;      %T1 of blood
+LT = 1.5;       %labelling duration
+SF = 32;        %scaling factor
 
+% Protocol dependent parameters, check if running new protocol
 % UMMAP NEX=3 PLD=1.525
 % PTR   NEX=3 PLD=2.025
 
-NEX=3;
+NEX = 3;
 
 % post labelling delay
-PLD=1.525;    %for UMMAP protocol
-PLD=2.025;    %for PTR protocol
+PLD = 1.525;    %for UMMAP protocol
+PLD = 2.025;    %for PTR protocol
 
 %% equation definitions
 eqnum = (1-exp(-ST/T1t))*exp(PLD/T1b);
 eqdenom = 2*T1b*(1-exp(-LT/T1b))*eff*NEX;
-
-% don't know what this is
-SF = 32;
 
 % final scaling factors
 cbf = 6000*alpha*(eqnum/eqdenom)*(pw./(SF*pd));
@@ -49,3 +52,10 @@ cbf = 6000*alpha*(eqnum/eqdenom)*(pw./(SF*pd));
 cbfmap = cbf;
 cbfmap(find(msk1)) = 0;
 cbfmap(find(msk2)) = 1;
+
+% Get original header, use it to write out cbfmap as nifti image
+h=read_nii_hdr(asl_filename);
+h2=h;
+h2.dim(5)=1;
+
+write_nii('cbfmap.nii', cbfmap, h2, 0)
