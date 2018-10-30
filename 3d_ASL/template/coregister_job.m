99158d2d0e77ebd6dab6f186c4ded0861da22540
sean@mbpro_new <tehsheng+mbpro_new@umich.edu>
2018-10-30 12:29:50 -0400

    add SPM12 Batch template script for coregistration

---
+++ 3d_ASL/template/coregister_job.m
@@ -0,0 +1,11 @@
+%-----------------------------------------------------------------------
+% Job saved on 30-Oct-2018 12:03:20 by cfg_util (rev $Rev: 6942 $)
+% spm SPM - SPM12 (7219)
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.spatial.coreg.estimate.ref = {'/Users/tehsheng/Dropbox/hlp17umm01220_03783/vasc_3dasl.nii,2'};
+matlabbatch{1}.spm.spatial.coreg.estimate.source = {'/Users/tehsheng/Dropbox/hlp17umm01220_03783/t1mprage_208.nii,1'};
+matlabbatch{1}.spm.spatial.coreg.estimate.other = {''};
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'ecc';
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.sep = [4 2];
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.fwhm = [7 7];
