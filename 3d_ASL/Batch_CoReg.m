5fe873c494bc11e8a610b5a48ea18507272e0df2
sean@mbpro_new <tehsheng+mbpro_new@umich.edu>
2018-10-30 13:37:10 -0400

    add brain mask reslicing section

--- 3d_ASL/Batch_CoReg.m
+++ 3d_ASL/Batch_CoReg.m
@@ -90,6 +90,35 @@ for iSubjDir = 1:size(SubjDir)
     system(betCommand);
     system(unzipCommand);
 
+    % ----- Reslice bet2 mask to vasc_3dasl section -----
+    fprintf('========================\n')
+    fprintf('  Reslicing brain mask  \n')
+    fprintf('========================\n')
+    %-----------------------------------------------------------------------
+    % Job saved on 30-Oct-2018 13:29:09 by cfg_util (rev $Rev: 6942 $)
+    % spm SPM - SPM12 (7219)
+    %-----------------------------------------------------------------------
+    matlabbatch{1}.spm.spatial.coreg.estwrite.ref = {strcat(SubjDirPath,'/vasc_3dasl.nii,2')};
+    matlabbatch{1}.spm.spatial.coreg.estwrite.source = {strcat(SubjDirPath,'bet_t1mprage_208_mask.nii,1')};
+    matlabbatch{1}.spm.spatial.coreg.estwrite.other = {''};
+    matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.cost_fun = 'ecc';
+    matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.sep = [4 2];
+    matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+    matlabbatch{1}.spm.spatial.coreg.estwrite.eoptions.fwhm = [7 7];
+    matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.interp = 4;
+    matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.wrap = [0 0 0];
+    matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.mask = 0;
+    matlabbatch{1}.spm.spatial.coreg.estwrite.roptions.prefix = 'reSlice_3dasl2_';
+
+    % spm initiation
+    spm_jobman('initcfg');
+    spm('defaults','fmri');
+    global defaults;
+
+    % running spm job
+    spm_jobman('run',matlabbatch);
+
+    clear matlabbatch
 
     % finding the printed .ps. rename and move it
     fprintf('==================================\n')
