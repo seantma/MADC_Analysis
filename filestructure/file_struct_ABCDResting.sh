af5370f286c32b9593a6b37ef67f6f53b3d16e33
Sean@benmac <tehsheng+benmac@umich.edu>
2018-11-21 11:59:11 -0500

    added subject specific issues for fs stage

--- filestructure/file_struct_ABCDResting.sh
+++ filestructure/file_struct_ABCDResting.sh
@@ -117,3 +117,21 @@ done < MADC_ABCDResting_1stbatch_Nov21_2018.csv
 ## ==== addressing specific subject issues ====
 #
 # grep -i 'cannot' Log_file_struct_ABCDResting_1121_2018.txt
+# hlp17umm01494_05012 - 1. FIXED by relinking!! has t1sag_208/ instead of t1mprage_208/;
+# hlp17umm01387_03659 - 1. FIXED by linking utrun_01.nii, meanutrun_01.nii !! no utprun_01.nii, no meanutprun_01.nii
+#
+# hlp17umm01549_05224 - 1. has t1sag_208/ instead of t1mprage_208/; 2. has only run_01.nii in /restingabcd
+# hlp17umm01543_05181 - 1. has t1sag_208/ instead of t1mprage_208/; 2. has only run_01.nii in /restingabcd
+# hlp17umm01539_05154 - 1. has t1sag_208/ instead of t1mprage_208/; 2. has only run_01.nii in /restingabcd
+# hlp17umm01532_05160 - 1. has t1sag_208/ instead of t1mprage_208/; 2. has only run_01.nii in /restingabcd
+# hlp17umm01508_05049 - 1. has t1sag_208/ instead of t1mprage_208/; 2. has utprun_01.nii but no realign.data in /restingabcd
+# hlp17umm01500_05193 - 1. has t1sag_208/ instead of t1mprage_208/; 2. has only run_01.nii in /restingabcd
+# hlp17umm01492_05182 - 1. has t1sag_208/ instead of t1mprage_208/; 2. has only run_01.nii in /restingabcd
+# hlp17umm01483_05200 - 1. has t1sag_208/ instead of t1mprage_208/; 2. has only run_01.nii in /restingabcd
+# hlp17umm01405_03782 - 1. NO anatomy!! yet completed scans!!
+# hlp17umm01371_05133 - 1. has t1sag_208/ instead of t1mprage_208/; 2. has only run_01.nii in /restingabcd
+# hlp17umm01346_03445 - 1. has t1sag_208/ instead of t1mprage_208/; 2. NO run_01.nii in restingabcd/
+# hlp17umm01252_05187 - 1. has t1sag_208/ instead of t1mprage_208/; 2. has only run_01.nii in /restingabcd
+# hlp17umm01246_05153 - 1. has t1sag_208/ instead of t1mprage_208/; 2. has only run_01.nii in /restingabcd
+# hlp17umm01151_04164 - 1. has t1sag_208/ instead of t1mprage_208/; 2. has only run_01.nii in /restingabcd
+# hlp17umm00773_03440 - 1. NO run_01.nii and realign.dat in /restingabcd!!
