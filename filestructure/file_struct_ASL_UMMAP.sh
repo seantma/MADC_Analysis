da736980ea88dbad72e70bc93148232f0fb11d2a
Sean@benmac <tehsheng+benmac@umich.edu>
2018-12-06 15:37:02 -0500

    echo out T1_File/T1_DIR

--- filestructure/file_struct_ASL_UMMAP.sh
+++ filestructure/file_struct_ASL_UMMAP.sh
@@ -73,7 +73,9 @@ do
 
   else
     cd ${T1_DIR}
+    echo ${T1_DIR}
     T1_FILE=$(ls ${RAWDIR}/${oldfolder}/anatomy/*.nii | grep -E 't1mprage|t1sag')
+    echo ${T1T1_FILE}
     # previously using cp -ip --> occupying too much space; using hardlinks instead
     # !!Note!! switching back to cp -ip for /anatomy files due to direct alterations
     # !!Note!! linking symbolic links first then do an actual cp -pL dereference copy
