2008355126ec9ebd4346e885c06c4b19a0e033a0
Sean@benmac <tehsheng+benmac@umich.edu>
2018-12-06 15:39:57 -0500

    debugging path issues

--- filestructure/file_struct_ASL_UMMAP.sh
+++ filestructure/file_struct_ASL_UMMAP.sh
@@ -74,13 +74,14 @@ do
   else
     cd ${T1_DIR}
     echo ${T1_DIR}
-    T1_FILE=$(ls ${RAWDIR}/${oldfolder}/anatomy/*.nii | grep -E 't1mprage|t1sag')
+    echo ${PWD}
+    T1_FILE=$(ls *.nii | grep -E 't1mprage|t1sag')
     echo ${T1T1_FILE}
     # previously using cp -ip --> occupying too much space; using hardlinks instead
     # !!Note!! switching back to cp -ip for /anatomy files due to direct alterations
     # !!Note!! linking symbolic links first then do an actual cp -pL dereference copy
     # adding || as try/catch mechanism
-    ln -s ${RAWDIR}/${oldfolder}/anatomy/${T1_DIR}/${T1_FILE} ${T1_FILE}
+    ln -s ${T1_DIR}/${T1_FILE} ${T1_FILE}
     cp -pL ${T1_FILE} t1spgr.nii  || echo "Error!! Subject: ${newfolder} has no t1spgr.nii !!"
 
   fi
