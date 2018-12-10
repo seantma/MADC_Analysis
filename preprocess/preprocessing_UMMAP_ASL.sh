8952056a5c76fb3166f2a797803c0cc474e5be66
Sean@benmac <tehsheng+benmac@umich.edu>
2018-12-10 16:17:31 -0500

    re-preprocess 1417,1458,1519 for aborted preprocessing

--- preprocess/preprocessing_UMMAP_ASL.sh
+++ preprocess/preprocessing_UMMAP_ASL.sh
@@ -26,70 +26,70 @@
 # Does not contain filtered out subjects from QC check
 
 subjIDs=(
-  #Batch 1
-  madc0798_5794
-  madc0799_5795
-  madc1040_5745
-  madc1182_5506
-  madc1220_3783
-  madc1243_4201
-  madc1246_5153
-  madc1252_5187
-  madc1263_3874
-  madc1276_3938
-  madc1299_5225
-  madc1307_5883
-  madc1320_5807
-  madc1348_5194
-  madc1350_5521
-  madc1371_5133
-  madc1373_5314
-  madc1384_5358
-  madc1387_5556
-  madc1389_3696
-  madc1395_5848
-  #Batch 2
-  madc1416_5640
+  # #Batch 1
+  # madc0798_5794
+  # madc0799_5795
+  # madc1040_5745
+  # madc1182_5506
+  # madc1220_3783
+  # madc1243_4201
+  # madc1246_5153
+  # madc1252_5187
+  # madc1263_3874
+  # madc1276_3938
+  # madc1299_5225
+  # madc1307_5883
+  # madc1320_5807
+  # madc1348_5194
+  # madc1350_5521
+  # madc1371_5133
+  # madc1373_5314
+  # madc1384_5358
+  # madc1387_5556
+  # madc1389_3696
+  # madc1395_5848
+  # #Batch 2
+  # madc1416_5640
   madc1417_5641
-  madc1435_5392
-  madc1446_4476
-  madc1447_4475
+  # madc1435_5392
+  # madc1446_4476
+  # madc1447_4475
   madc1458_4524
-  madc1462_4510
-  madc1476_5633
-  # madc1477_5635   #no T1 in original folder
-  madc1483_5200
-  madc1487_4682
-  madc1492_5182
-  madc1500_5193
-  madc1508_5049
-  madc1511_5424
-  madc1513_5315
+  # madc1462_4510
+  # madc1476_5633
+  # # madc1477_5635   #no T1 in original folder
+  # madc1483_5200
+  # madc1487_4682
+  # madc1492_5182
+  # madc1500_5193
+  # madc1508_5049
+  # madc1511_5424
+  # madc1513_5315
   madc1519_5232
-  madc1530_5703
-  madc1531_5159
-  madc1532_5160
-  madc1535_5853
-  madc1537_5308
-  # Batch 3
-  madc1539_5154
-  madc1543_5181
-  madc1544_5549
-  madc1545_5550
-  # madc1546_5697   #no T1 in original folder
-  madc1549_5224
-  madc1550_5702
-  madc1550_5786
-  madc1555_5471
-  madc1556_5503
-  madc1557_5890
-  madc1561_5696
-  madc1564_5806
-  madc1565_5522
-  madc1572_5785
-  madc1575_5746
-  madc1588_5891
-  madc1595_5847
+  # madc1530_5703
+  # madc1531_5159
+  # madc1532_5160
+  # madc1535_5853
+  # madc1537_5308
+  # # Batch 3
+  # madc1539_5154
+  # madc1543_5181
+  # madc1544_5549
+  # madc1545_5550
+  # # madc1546_5697   #no T1 in original folder
+  # madc1549_5224
+  # madc1550_5702
+  # madc1550_5786
+  # madc1555_5471
+  # madc1556_5503
+  # madc1557_5890
+  # madc1561_5696
+  # madc1564_5806
+  # madc1565_5522
+  # madc1572_5785
+  # madc1575_5746
+  # madc1588_5891
+  # madc1595_5847
 )
 
 for SUBJECT in ${subjIDs[@]}
