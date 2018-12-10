bebcf6ad81cf1e64007180e9253c3864aa801e06
Sean@madc <tehsheng+madc@umich.edu>
2018-12-10 15:54:58 -0500

    add preprocess console logs for UMMAP 3dASL Dec2018 Baseline batch

---
+++ preprocess/spm8Batch/coregOverlay/2018_12/coregOverlay_181208_00_50_54_535_tehsheng_madcbrai.sh
@@ -0,0 +1,115 @@
+#!/bin/bash -l
+#
+# coregOverlay Version 2.1 2011-07-29
+# Copyright Robert C. Welsh
+#
+#
+STARTTIME=`date` 
+#
+#     anatomy path             : anatomy/
+#     directory to coreg       : func//coReg//
+#     functional sub-dir       : 
+#     functional images        : vasc_3dasl
+#     name of overlay image    : t1spgr
+#     reslice option           : 0
+#     prepending name          : 
+#     Other images             : 0
+
+
+#     functional images path    : func/
+#     Subject directory         : ../../Subjects_ASL
+
+#     Back(1)/fore(0)ground     : 0
+
+#     spm8 is located in        : /nfs/psych-bhampstelab/VA_SPiRE_2015/fMRI_Working/MCore/SPM/SPM8/spm8_with_R6313
+#     spm8Batch is located in   : /nfs/psych-bhampstelab/VA_SPiRE_2015/fMRI_Working/MCore/spm8Batch
+#     spm8 patch is located in  : /nfs/psych-bhampstelab/VA_SPiRE_2015/fMRI_Working/MCore/spm8Batch/matlabScripts
+#     auxiliary matlab path     : /nfs/psych-bhampstelab/VA_SPiRE_2015/fMRI_Working/MCore/spm8Batch/spm8_patch
+#     matlab options file       : 
+
+#     User                      : NOMAIL
+#     MATLAB                    : /opt/bin/matlab
+
+#     SANDBOXHOST               : madcbrain
+#     SANDBOX                   : /madcbrain/sandbox/
+#     SANDBOXPID                : 
+
+#     UMSTREAM_STATUS_FILE      : /nfs/fmri/Analysis/Sean_Working/Git_MADC/preprocess/madc1550_5702_validation_umstream_status_file
+
+#     Copyright Robert C. Welsh, 2005-2018, Version 2.1/2011-07-29
+
+MATLAB_BASED_PROCESS=0   # Default to not a matlab process
+
+# Get into the right working directory
+cd /nfs/fmri/Analysis/Sean_Working/Git_MADC/preprocess
+#
+MATLAB_BASED_PROCESS=1   # We are indeed a matlab based process
+
+/opt/bin/matlab -nodisplay <<EOF 
+
+% coregOverlay Script Created on 181208_00_50_54_535
+
+fprintf('Coregistation Script Created on 181208_00_50_54_535\n');
+
+addpath /nfs/psych-bhampstelab/VA_SPiRE_2015/fMRI_Working/MCore/SPM/SPM8/spm8_with_R6313
+addpath /nfs/psych-bhampstelab/VA_SPiRE_2015/fMRI_Working/MCore/spm8Batch
+addpath /nfs/psych-bhampstelab/VA_SPiRE_2015/fMRI_Working/MCore/spm8Batch/matlabScripts
+addpath /nfs/psych-bhampstelab/VA_SPiRE_2015/fMRI_Working/MCore/spm8Batch/spm8_patch
+addpath /Users/rcwelsh/matlabScripts/WELLES/mfiles -END
+addpath /nfs/fmri/Analysis/Sean_Working/Git_MADC/preprocess/spm8Batch/coregOverlay/2018_12
+
+global UMBatchJobName
+global UMBatchProcessName
+UMBatchJobName     = 'coregOverlay_181208_00_50_54_535_tehsheng_madcbrai'
+UMBatchProcessName = 'coregOverlay'
+
+coregOverlay_181208_00_50_54_535_tehsheng_madcbrai
+
+exit
+EOF
+MATLAB_RETURN_CODE=$?
+
+JOBSTATUS="SUCCESS"
+
+if [ "${MATLAB_BASED_PROCESS}" == "1" ]
+then
+    if [ "${MATLAB_RETURN_CODE}" != "0" ] 
+    then
+	JOBSTATUS="MATLAB_FAILED:${MATLAB_RETURN_CODE}"
+    fi
+fi
+if [ ! -z "/nfs/fmri/Analysis/Sean_Working/Git_MADC/preprocess/madc1550_5702_validation_umstream_status_file" ] 
+then
+   echo "coregOverlay ${JOBSTATUS}, attempted at : 181208_00_50_54_535" >> ${UMSTREAM_STATUS_FILE}
+fi
+
+#
+# Remove the sandbox
+#
+/nfs/psych-bhampstelab/VA_SPiRE_2015/fMRI_Working/MCore/spm8Batch/auxiliary/remove_sandbox  
+#
+ENDTIME=`date`
+#
+#
+echo   
+echo   
+echo "Start   : ${STARTTIME}" 
+echo "Finish  : ${ENDTIME}" 
+echo  
+echo  
+#
+#
+# This job was created on 181208_00_50_54_535 with the command line:
+#
+# /nfs/psych-bhampstelab/VA_SPiRE_2015/fMRI_Working/MCore/spm8Batch/coregOverlay -M ../../Subjects_ASL madc1550_5702 -v vasc_3dasl -o t1spgr -B
+#
+# Command was built while in the directory:
+#
+# /nfs/fmri/Analysis/Sean_Working/Git_MADC/preprocess
+#
+# spm8Batch Version : spm8Batch VBM8 2013-04-01 On branch spm8Batch/Fix_sandbox_on_MAC
+# MethodsCore Release Tag : MethodsCore 2.0
+# MethodsCore Repository SHA-ID: 
+#
+# All done
+#
---
+++ preprocess/spm8Batch/warpfMRI/2018_12/warpfMRI_181207_21_22_21_056_tehsheng_madcbrai.m
@@ -0,0 +1,164 @@
+%
+% Copyright Robert C. Welsh
+% Ann Arbor, Michigan 2005
+% 
+% This is to be used with the UMBatch System for SPM2.
+%
+% To Warp image  using batch use the following code.
+%
+
+% You must point to the batch processing code
+%
+%  addpath /net/dysthymia/spm8Batch
+%
+
+% You need to fill the following variables:
+%
+%   UMBatchMaster  =   point to the directory of the experiment.
+%
+%   TemplateImage  =   image that represents the normalized space.
+%                      if [], then no determination made.
+%
+%   UMImg2Warp     =   A {} array of image names to warp to the 
+%                      standard template.
+%
+%   ParamImage     =   image for determining the normalization.
+%
+%   ObjectMask     =   image mask for warping - default is [];
+%
+%   Images2Write   =   image to warp.
+%                      if [], then no images to write normalize.
+%
+% 
+%        If you are needing to write alot of images out I suggest you use 
+%        a command like this:
+%  
+%              Images2Write = spm_get('files',ImagesDir,'ravol*.img');
+%
+%        where "ImagesDir" is the directory where the images live. You can of 
+%        course put a loop around to write normalize each run seperately etc.
+%
+%   UMBatchSubjs   =   list of subjects within experiment. Space
+%                      pad if need be.
+%
+
+
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+% Make sure the UM Batch system is installed.
+
+if exist('UMBatchPrep') ~= 2 | exist('UMBatchWarp') ~= 2
+    fprintf('You need to have the UM Batch system\n');
+    return
+end
+
+% 
+% Prepare the batch processes
+%
+
+results = UMBatchPrep;
+
+if UMCheckFailure(results)
+  exit(abs(results));
+end
+
+
+% - - - - - - - END OF PART I - - - - - - - - - - - - - - - - -
+
+
+ 
+% This part placed by 'warpfMRI' auto script
+ 
+% 
+% 
+% This script is called :  
+% 
+% /nfs/fmri/Analysis/Sean_Working/Git_MADC/preprocess/spm8Batch/warpfMRI/2018_12/warpfMRI_181207_21_22_21_056_tehsheng_madcbrai.m
+% 
+% and create on 181207_21_22_21_056
+% 
+ 
+UMBatchMaster = '/nfs/fmri/Analysis/Sean_Working/Git_MADC/preprocess';
+ 
+
+% If the abort file varialbe is set then we
+% can write status of the failure there.
+
+global UMBatchStatusFile
+UMBatchStatusFile = '/nfs/fmri/Analysis/Sean_Working/Git_MADC/preprocess/madc1575_5746_validation_umstream_status_file';
+
+UMTestFlag    = 0;
+TemplateImage  = '/nfs/psych-bhampstelab/VA_SPiRE_2015/fMRI_Working/MCore/SPM/SPM8/spm8_with_R6313/templates//T1.nii';
+VoxelSize      = 0;
+WARPMETHOD     = 1;
+VBM8RefImage   = '/nfs/psych-bhampstelab/VA_SPiRE_2015/fMRI_Working/MCore/SPM/SPM8/spm8_with_R6313/templates//PET.nii';
+OutputName     = 'vbm8_w2mm_';
+SandBoxDIR     = '/madcbrain/sandbox//';
+SandBoxPID     = '/';
+UMBatchSubjs  = {...
+      'madc1575_5746';...
+                 };
+ 
+UMVolumeWild  = 'vasc_3dasl';
+UMImgDIRS    = {...
+                 {'/nfs/fmri/Analysis/Sean_Working/Subjects_ASL/madc1575_5746/func/run_01'};...
+                };
+ UMHighRes    = {...
+'/nfs/fmri/Analysis/Sean_Working/Git_MADC/preprocess/../../Subjects_ASL/madc1575_5746/func//coReg/vbm8/t1spgr.nii';...
+                };
+% - - - - - - - -BEGINNING OF PART II - - - - - - - - - - - - -
+
+% Deblank just in case.
+
+UMBatchMaster = strtrim(UMBatchMaster);
+
+% - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+fprintf('Looping on the subjects now\n\n');
+
+%
+% Loop over the subjects and coregister each one.
+%
+
+for iSub = 1:length(UMBatchSubjs)
+  %
+  fprintf('Working on %s\n',UMBatchSubjs{iSub})
+  % 
+  % Use the subjects SPGR that was previously normalized
+  % to write normalize the fMRI volumes.
+  %
+  ParamImage = UMHighRes{iSub};
+  % Loop on the runs and get the names of the files to normalize.
+  % We use the "spm_get('files',[directory],[file wildcard])" command
+  % to get the list of files.
+  %
+  for iRun = 1:length(UMImgDIRS{iSub})
+    %
+    % Find out if they are using a sandbox for the warping.
+    %
+    [CS SandBoxPID Images2Write] = moveToSandBox(UMImgDIRS{iSub}{iRun},UMVolumeWild,SandBoxPID);
+    nFiles = size(Images2Write,1);
+    fprintf('Normalizing a total of %d %s''s\n',nFiles,UMVolumeWild);
+    % 
+    % The other step to apply the normalization to the SPGR.
+    %
+    if WARPMETHOD
+      results = UMBatchWarp([],ParamImage,VBM8RefImage,Images2Write,UMTestFlag,VoxelSize,OutputName,WARPMETHOD);      
+    else
+      results = UMBatchWarp([],ParamImage,[],Images2Write,UMTestFlag,VoxelSize,OutputName,WARPMETHOD);      
+    end
+    if UMCheckFailure(results)
+      exit(abs(results));
+    end
+    %
+    % Now move back out of sandbox if so specified
+    %
+    CSBACK = moveOutOfSandBox(UMImgDIRS{iSub}{iRun},UMVolumeWild,SandBoxPID,OutputName,CS);
+  end
+end
+
+fprintf('\nAll done with warping of fMRIs to template using HiResolution data.\n');
+
+%
+% All done.
+%
